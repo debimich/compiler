d94008e7026a13fbcfaee9915845d1bfb84440ba
debimich <debimich@MSI>
2024-04-23 16:23:04 +0300

    Modified files

--- compiler.cpp
+++ compiler.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cstring>
+#include <stack>
+#include <string>
 
 struct state
 {
@@ -7,18 +8,8 @@ struct state
 	int S;
 };
 
-struct stack
-{
-	int nums[100];
-	int top;
-};
-
-int scanner(char *res, char *inp);
-char terminals(char *term);
-void init(stack *stk);
-void push(stack *stk, int num);
-int pop(stack *stk);
 int transform(char token);
+int addSpaces(std::string *str);
 
 int main()
 {
@@ -35,94 +26,38 @@ int main()
 						   {{-1, -1}, {-1, -1}, {-1, -1}, {11, -1}, {-1, -1}, {-1, -1}, {-1, -1}},	// 10
 						   {{-1, -1}, {12, -1}, {-1, -1}, {-1, -1}, {12, -1}, {-1, -1}, {-1, -1}},	// 11
 						   {{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-2, -2}, {-2, -2}}}; // 12
-	stack stk;
-	init(&stk);
-	char string[100];
+	std::stack<int> stack;
+	std::string str;
 	std::cout << "Input string:" << std::endl;
-	std::cin >> string;
-	while (strcmp(string, "end") != 0)
+	std::getline(std::cin, str);
+	while (str != "end")
 	{
+		addSpaces(&str);
 		int qpre = 0;
 		int q = 0;
 		int ind = 0;
 		while (q != -1 && q != 15)
 		{
 			qpre = q;
-			q = states[q][transform(string[ind])].q;
+			q = states[q][transform(str[ind])].q;
 			if (q == 2)
-				push(&stk, states[qpre][transform(string[ind])].S);
+				stack.push(states[qpre][transform(str[ind])].S);
 			else if (q == -2)
-				q = pop(&stk);
+			{
+				q = stack.top();
+				stack.pop();
+			}
 			else
 				ind++;
 		}
-		if (q == 15 && ind == strlen(string))
+		if (q == 15 && ind == str.length())
 			std::cout << "Correct" << std::endl;
 		else
 			std::cout << "Failed" << std::endl;
 		std::cout << "Input string:" << std::endl;
-		std::cin >> string;
+		std::cin >> str;
 	}
 	return 0;
-}
-
-int addSpaces(char *res, char *inp)
-{
-	
-}
-
-int scanner(char *res, char *inp)
-{
-	char *temp = strtok(inp, " ");
-	char *temp2;
-	while (temp != NULL)
-	{
-		if (terminals(temp) != '0')
-			snprintf(res, strlen(res), "%s", terminals(temp));
-		else
-		{
-			temp2 = strstr(temp, ":=");
-			if (temp2 == NULL)
-			{
-				// Identifier();
-			}
-		}
-		temp = strtok(NULL, " ");
-	}
-}
-
-char terminals(char *term)
-{
-	if (strcmp(term, "if") == 0)
-		return 'f';
-	if (strcmp(term, "then") == 0)
-		return 't';
-	if (strcmp(term, "else") == 0)
-		return 'e';
-	if (strcmp(term, ";") == 0)
-		return ';';
-	if (strcmp(term, ":=") == 0)
-		return 'a';
-	return '0';
-}
-
-void init(stack *stk)
-{
-	stk->top = 0;
-}
-
-void push(stack *stk, int num)
-{
-	stk->nums[stk->top] = num;
-	stk->top++;
-}
-
-int pop(stack *stk)
-{
-	int num;
-	stk->top--;
-	num = stk->nums[stk->top];
-	return num;
 }
 
 int transform(char token)
@@ -154,4 +89,25 @@ int transform(char token)
 		return -1;
 		break;
 	}
+}
+
+int addSpaces(std::string *str)
+{
+	size_t index{};
+	while ((index = str->find(":=", index)) != std::string::npos)
+	{
+		std::cout << index << std::endl;
+		if (str->substr(index - 1, 1) != " ")
+		{
+			str->insert(index, " ");
+			index++;
+		}
+		if (str->substr(index + 2, 1) != " ")
+		{
+			str->insert(index + 2, " ");
+			index++;
+		}
+		index += 2;
+	}
+	return 0;
 }
